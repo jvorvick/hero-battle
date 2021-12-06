c72b9e42ed27df46ca4ce194eef58bd963b61c38
jvorvick <49791235+jvorvick@users.noreply.github.com>
2021-12-05 21:45:50 -0800

    requirements and analysis

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+This is a game where a hero will fight a monster, taking turns, until one has 0 hit points. Each attack will do a random amount of damage based on the entity's strength, and will have a random chance of success based on the defender's dexterity. Fight until death.
+
+Nouns: game, hero, monster, attacker, defender, entity
+Adjectives: strength, hp, dead, turn
+Verbs: fight, attack
+'''
+
