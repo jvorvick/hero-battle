fdcb03cf0a19000637428de7de87aeed9ae3cdc1
jvorvick <49791235+jvorvick@users.noreply.github.com>
2022-05-13 16:28:51 -0700

    refactored maploader

--- map_loader.py
+++ map_loader.py
@@ -1,25 +1,37 @@
-f = open('map.txt', 'r')
-lines = f.readlines()
-rows = []
-for line in lines:
-    row = []
-    for i in range(0, len(line), 2):
-        c = line[i]
-        row.append(c)
-    rows.append(row)
-print(rows)
+class MapLoader:
+    def __init__(self, map_name = 'map.txt'):
+        self.map_name = map_name
+        self.rows = self.load()
 
-for r in rows:
-    for c in r:
-        print(c, end='-')
-    print('')
+    def load(self):
+        f = open(self.map_name, 'r')
+        lines = f.readlines()
+        rows = []
+        for line in lines:
+            row = []
+            for i in range(0, len(line), 2):
+                c = line[i]
+                row.append(c)
+            rows.append(row)
+        return rows
 
-entities = []
-for ir, r in enumerate(rows):
-    for ic, c in enumerate(r):
-        entities.append({
-            'x': ic,
-            'y': ir,
-            'v': c
-        })
-print(entities)
+    def display(self):
+        for r in self.rows:
+            for c in r:
+                print(c, end='-')
+            print('')
+
+    def get_entities(self):
+        entities = []
+        for ir, r in enumerate(self.rows):
+            for ic, c in enumerate(r):
+                entities.append({
+                    'x': ic,
+                    'y': ir,
+                    'v': c
+                })
+        return entities
+
+ml = MapLoader('map.txt')
+ml.display()
+print(ml.get_entities())
